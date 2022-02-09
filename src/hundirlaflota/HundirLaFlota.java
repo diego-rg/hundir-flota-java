5d3973383afc8959e9847cd18c0963d3327d5ea7
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-02-09 16:37:33 +0100

    mapas, puntuaciones, condiciones fin partida

--- src/hundirlaflota/HundirLaFlota.java
+++ src/hundirlaflota/HundirLaFlota.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Juego de hundir la flota
  */
 package hundirlaflota;
 
@@ -8,15 +8,51 @@ import java.util.Scanner;
 
 /**
  * @author Diego
- * Juego de hundir la flota
+ * 
  */
 public class HundirLaFlota {
+	//Constantes para indicar tipo de casilla
+	final static char AGUA_NO_TOCADO = '.';
+	final static char AGUA = 'A';
+	final static char TOCADO = 'X';
 
+	//TAMAÑO DEL TABLERO
+	final static int TAMANO = 10;
+
+	private static Scanner sc;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Permiten input del teclado
+		sc = new Scanner(System.in);
+
+		//Creación de mapas
+		char[][] mapaUsuario = new char[TAMANO][TAMANO];
+		char[][] mapaOrdenador = new char[TAMANO][TAMANO];
+
+		//Mapa para ver las tiradas del pc
+		char[][] mapaOrdenadorParaUsuario = new char[TAMANO][TAMANO];
+
+		// Puntuación inicial, fin de partida en el primero a 0		
+		int puntosUsuario = 24;
+		int puntosOrdenador = 24;
+
+		//Controla fin de partida cuando el primero llega a 0
+		boolean juegoTerminado = false;
+
+		//Si la tirada es correcta inicia otra
+		boolean tiroCorrecto = false;
+
+		//Posiciones de la tirada
+		int[] tiro = new int[2];
+
+		//Inicializamos los mapas generando los barcos
+		inicializacion(mapaUsuario, mapaOrdenador);
+		
+		//Inicializamos el mapa de tiradas al pc
+		inicializaMapaRegistro(mapaOrdenadorParaUsuario);
 
 	}
 
