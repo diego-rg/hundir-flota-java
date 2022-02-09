4af9c57cf2775ae1be9b24292fe53ff72b8f131b
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-02-09 16:16:25 +0100

    creación proyecto, clase, package, etc

---
+++ src/hundirlaflota/HundirLaFlota.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package hundirlaflota;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author Diego
+ * Juego de hundir la flota
+ */
+public class HundirLaFlota {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
